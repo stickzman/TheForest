1d487fef96405e21a19756c2aae128e4246c96e3
Dan <djahl2014@gmail.com>
2014-12-16 16:00:06 -0500

    Created objects.js and added it to game.html

---
+++ objects.js
@@ -0,0 +1,3 @@
+//This file contains the constructor and other functions related to objects.
+//Objects are distinct from items as they can be interacted with but not picked up.
+//Players can use an item on an object or interact with the object directly.
